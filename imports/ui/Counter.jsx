b316edc66f36ce8bffed478706fdab209ec62c67
Brooklin Myers <Brooklinjazz@hotmail.com>
2019-02-15 00:54:03 -0800

    add Material ui button to counter

--- imports/ui/Counter.jsx
+++ imports/ui/Counter.jsx
@@ -1,15 +1,18 @@
 import React, { useState } from 'react';
 import Todos from '../api/todos';
 import { withTracker } from 'meteor/react-meteor-data';
+import Button from '@material-ui/core/Button';
 
 function Counter(props) {
   const [counter, setCounter] = useState(0)
   // const [todos, setTodo] = useState(
-    console.log(props.todos);
+  console.log(props.todos);
   // )
   return (
     <div>
-      <button onClick={() => setCounter(counter + 1)}>Click Me</button>
+      <Button onClick={() => setCounter(counter + 1)} variant="contained">
+        Click Me
+      </Button>
       <p>You've pressed the button {counter} times.</p>
     </div>
   )
